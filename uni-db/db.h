673b702feefc33713c33061e689bcebbcdbac0c1
Nhat Nguyen <nhatnguyen.sg.vn@gmail.com>
2018-06-12 17:55:57 +0700

    Update header for db

--- uni-db/db.h
+++ uni-db/db.h
@@ -6,13 +6,10 @@
 #include <string>
 
 
-std::vector<std::vector<std::string> > SelectQuery(std::string query);
-
-
-void InsertQuery(std::string query);
-
-
 void DeleteQuery(std::string query);
+void InsertQuery(std::string query);
+std::vector<std::vector<std::string> > SelectQuery(std::string query);
+void SetQuery(std::string query);
 
 
 #endif // DB_H
